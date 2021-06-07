602344fc7fd3b41286b5e4535b4ebe40d0822663
MarianoCaccavale <75137536+MarianoCaccavale@users.noreply.github.com>
2021-06-07 11:26:48 +0200

    Update cropper.dart

--- lib/src/cropper.dart
+++ lib/src/cropper.dart
@@ -108,7 +108,7 @@ class ImageCropper {
     print('Mariano.py - width: ' + splitResult[3]);
     double tmpAngle = double.parse(splitResult[5]);
     print('Mariano.py - cropper.dart tmpAngle: ' + (tmpAngle).toString());
-    print('Mariano.py - cropper.dart tmpAngleInt: ' + (tmpAngle2.toInt()).toString());
+    print('Mariano.py - cropper.dart tmpAngleInt: ' + (tmpAngle.toInt()).toString());
     
 
     return CropInfo(
